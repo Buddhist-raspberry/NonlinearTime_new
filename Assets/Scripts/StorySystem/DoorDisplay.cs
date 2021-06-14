20f1293d565e6f5f70fa6afd27bb35993996062a
T20210430 <529914423@qq.com>
2021-06-14 21:46:02 +0800

    level_4 setting

---
+++ Assets/Scripts/StorySystem/DoorDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDisplay : StoryItemBase
+{ 
+    public GameObject door;
+
+    protected override void executeEvent()
+    {
+        Debug.Log(text);
+        door.SetActive(true);
+    }
+}
