abe799ddca981f505ce1b52cbaf0306649ddc906
Amonkey00 <892542697@qq.com>
2021-06-15 17:43:11 +0800

    Level02 Add UI Silders

--- Assets/Scripts/Level/Heat.cs
+++ Assets/Scripts/Level/Heat.cs
@@ -7,9 +7,9 @@ public class Heat : MonoBehaviour
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.layer==LayerMask.NameToLayer("Player")){
-            GameLogic.instance.Die();
-            //HealthController playerHealth = col.gameObject.GetComponent<HealthController>();
-            //playerHealth.ChangeHealth(-1*Mathf.FloorToInt(playerHealth.currentHealth)-1);
+            
+            PlayerProperty playerHealth = col.gameObject.GetComponent<PlayerProperty>();
+            playerHealth.reduceHP(3000);
         }
     }
 }
